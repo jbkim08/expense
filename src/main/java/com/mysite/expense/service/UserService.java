39f1ed86a08ef606933f81990075ed348796bb90
JBKIM <drv98@naver.com>
2024-05-21 10:46:49 +0900

    비용에 유저추가

--- src/main/java/com/mysite/expense/service/UserService.java
+++ src/main/java/com/mysite/expense/service/UserService.java
@@ -5,6 +5,8 @@ import com.mysite.expense.entity.User;
 import com.mysite.expense.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,16 @@ public class UserService {
         user.setUserId(UUID.randomUUID().toString()); //중복되지 않는 userID 생성
         userRepo.save(user);
     }
+
+    //로그인 된 유저 정보 가져오기
+    public User getLoggedInUser(){
+        //시큐리티에서 로그인된 인증 auth 가져오기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String loginUserEmail = auth.getName();
+        return userRepo.findByEmail(loginUserEmail).orElseThrow(()->
+                new RuntimeException("이메일을 찾을 수 없습니다"));
+    }
+
 
     //DTO => Entity
     private User mapToEntity(UserDTO userDTO) {
